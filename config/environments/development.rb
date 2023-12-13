316c2a817dd8f4bfba254b321db520bd0f29a8d8
Mohit Bodkhe <mobodkhe@bestpeers.com>
2023-12-13 17:32:19 +0530

    configration hostname and protocol

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "movie-buzz.onrender.com", protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
